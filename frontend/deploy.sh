74bdfbd33525f1fc677a51fdd87ef3ed75677df8
Estragon <124503629+CodingByEstragon@users.noreply.github.com>
2026-01-20 22:17:02 +0100

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -2,6 +2,6 @@ git checkout master
 
 npm run build
 
-scp -r build/* root@nodejs01.intern.promatis.de:/var/nodejs01.intern.promatis.de
+scp -r build/* root@nodejs01.intern.xxxxxxxx.de:/var/nodejs01.intern.xxxxxxxx.de
 
 echo "Done!"
